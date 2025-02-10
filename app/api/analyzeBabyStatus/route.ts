8d16274abb0cd97b94eab3f519c4fd5c5e99291e
Jingnan Li <jingnanl@amazon.com>
2025-02-10 17:44:50 +0800

    use NEXT_PUBLIC

--- app/api/analyzeBabyStatus/route.ts
+++ app/api/analyzeBabyStatus/route.ts
@@ -2,39 +2,39 @@ import { BedrockRuntimeClient, InvokeModelCommand } from '@aws-sdk/client-bedroc
 import { DetectFacesCommand, RekognitionClient } from "@aws-sdk/client-rekognition";
 import { NextResponse } from 'next/server';
 
-// // 添加调试日志
-// console.log('Environment variables check:', {
-//   accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID?.slice(0, 5), // 只打印前几位，避免泄露
-//   secretKeyExists: !!process.env.MY_AWS_SECRET_ACCESS_KEY,
-//   region: process.env.MY_AWS_REGION
-// });
+// 添加调试日志
+console.log('Environment variables check:', {
+  accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID?.slice(0, 5), // 只打印前几位，避免泄露
+  secretKeyExists: !!process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
+  region: process.env.NEXT_PUBLIC_AWS_REGION
+});
 
-// const accessKeyId = process.env.MY_AWS_ACCESS_KEY_ID;
-// const secretAccessKey = process.env.MY_AWS_SECRET_ACCESS_KEY;
-// const region = process.env.MY_AWS_REGION || 'ap-southeast-1';
+const accessKeyId = process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY;
+const region = process.env.NEXT_PUBLIC_AWS_REGION || 'ap-southeast-1';
 
-// // 添加更严格的验证
-// if (!accessKeyId || !secretAccessKey) {
-//   throw new Error('AWS Credentials are missing or invalid');
-// }
+// 添加更严格的验证
+if (!accessKeyId || !secretAccessKey) {
+  throw new Error('AWS Credentials are missing or invalid');
+}
 
-// if (accessKeyId.trim() === '' || secretAccessKey.trim() === '') {
-//   throw new Error('AWS Credentials cannot be empty strings');
-// }
+if (accessKeyId.trim() === '' || secretAccessKey.trim() === '') {
+  throw new Error('AWS Credentials cannot be empty strings');
+}
 
-// const awsConfig = {
-//   region,
-//   credentials: {
-//     accessKeyId,
-//     secretAccessKey
-//   }
-// };
+const awsConfig = {
+  region,
+  credentials: {
+    accessKeyId,
+    secretAccessKey
+  }
+};
 
-// const bedrockClient = new BedrockRuntimeClient(awsConfig);
-// const rekognitionClient = new RekognitionClient(awsConfig);
+const bedrockClient = new BedrockRuntimeClient(awsConfig);
+const rekognitionClient = new RekognitionClient(awsConfig);
 
-const bedrockClient = new BedrockRuntimeClient({ region: 'ap-southeast-1' });
-const rekognitionClient = new RekognitionClient({ region: 'ap-southeast-1' });
+// const bedrockClient = new BedrockRuntimeClient({ region: 'ap-southeast-1' });
+// const rekognitionClient = new RekognitionClient({ region: 'ap-southeast-1' });
 
 
 export async function POST(request: Request) {
